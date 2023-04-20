9c5fe637d7e0da24ea92c406f32a7d266805035d
Abdurrahman1299 <abdurrahmanms09@gmail.com>
2023-04-20 16:24:50 +0200

    apr.20

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -2,7 +2,10 @@ import React from "react";
 import "./ChartBar.css";
 
 export default function ChartBar(props) {
-  const barFillHeight = Math.round(props.value * props.maxValue * 100) + "%";
+  let barFillHeight = "0%";
+  if (props.maxValue > 0) {
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
+  }
   return (
     <div className="chart-bar">
       <div className="chart-bar__inner">
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 export default function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -19,6 +20,7 @@ export default function Expenses(props) {
   return (
     <div>
       <Card className="expenses">
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesFilter
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
